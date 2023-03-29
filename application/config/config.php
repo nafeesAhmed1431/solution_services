c68c58782f4c5eb2d8acd6005df321be0257d028
Nafees Ahmed <nafees1431@gmail.com>
2023-03-29 14:09:32 +0500

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/solution-services';
+$config['base_url'] = 'http://localhost/solution_services';
 // $config['base_url'] = 'https://dtss.miapprd.com';
 
 /*
